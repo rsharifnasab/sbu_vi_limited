442304f23eb1a4db38516495fced4cbcddb61d2c
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-01-26 12:03:33 +0330

    function haye kamel shode ro bordim akhar e vim

--- src/vi_limited/Vim.java
+++ src/vi_limited/Vim.java
@@ -3,22 +3,22 @@ package vi_limited;
 import java.io.File;
 
 /**
-	out main class for vim editor
+out main class for vim editor
 **/
 public class Vim{
 
 	/**
-		the tricky up and down (gharardad kardim!)
-		they are calculated like this:
-		up = ord(esc) + ord ([) + ord(A) + 10
-		up = ord(esc) + ord ([) + ord(B) + 10
-		up = ord(esc) + ord ([) + ord(c) + 10
-		up = ord(esc) + ord ([) + ord(AD) + 10
+	the tricky up and down (gharardad kardim!)
+	they are calculated like this:
+	up = ord(esc) + ord ([) + ord(A) + 10
+	up = ord(esc) + ord ([) + ord(B) + 10
+	up = ord(esc) + ord ([) + ord(c) + 10
+	up = ord(esc) + ord ([) + ord(AD) + 10
 
-		check about ansi escape codes
+	check about ansi escape codes
 
-		why +10? because set them out of 0-127 scope
-		and also 193-196 are a bit meaningful
+	why +10? because set them out of 0-127 scope
+	and also 193-196 are a bit meaningful
 	**/
 	public static final char up = (char)193;
 	public static final char down = (char)194;
@@ -26,8 +26,8 @@ public class Vim{
 	public static final char left = (char)196;
 
 	/**
-		height and width of whole editor
-		they are fixed because it is not possible to find terminal size in java!
+	height and width of whole editor
+	they are fixed because it is not possible to find terminal size in java!
 	**/
 
 	public final int height = 24 - 4; // TODO test mode
@@ -38,40 +38,40 @@ public class Vim{
 	public final Screen screen;
 
 	/**
-		this will determine current editor mode
-		command mode for command
-		insert mode for edit
-		and etc..
+	this will determine current editor mode
+	command mode for command
+	insert mode for edit
+	and etc..
 	**/
 	public EditorMode mode;
 
 	/**
-		the command that is entering and not fully enterred yet
-		if user prees enter, we apply this command and reset this string
+	the command that is entering and not fully enterred yet
+	if user prees enter, we apply this command and reset this string
 
 	**/
 	public String command;
 
 	/**
-		a boolean to determine the app sohuld
-		continue to run
-		or it should exit
+	a boolean to determine the app sohuld
+	continue to run
+	or it should exit
 
-		exit() method will make this false;
+	exit() method will make this false;
 	**/
 	private Boolean running = true;
 
 	/**
-		first cunstructor of vim class
-		change with caution
-		it make new instance of cursor and screen class
-		checkout the commands order, they are not random
+	first cunstructor of vim class
+	change with caution
+	it make new instance of cursor and screen class
+	checkout the commands order, they are not random
 
-		we do following things :
-			set our file (if any)
-			make terminal handy with decresing buffer to one in order to use terminal as editor!
-			greet uesr ( totally optional )
-			clean cunsole and print all of screen (that should be initailized with ' ')
+	we do following things :
+	set our file (if any)
+	make terminal handy with decresing buffer to one in order to use terminal as editor!
+	greet uesr ( totally optional )
+	clean cunsole and print all of screen (that should be initailized with ' ')
 	**/
 	public Vim(String ourFile){
 		Logger.log("starting vim app");
@@ -94,47 +94,23 @@ public class Vim{
 	}
 
 	/**
-		second cunstrctor of vim class
-		it give args as paramter to parse it later and extract filename
+	second cunstrctor of vim class
+	it give args as paramter to parse it later and extract filename
 	**/
 	public Vim(String[] args){
 		this( ETCUtil.getFileNameFromArgs(args) );
 	}
 
-	/**
-		this method do nothing except greeting user and
-		waste his/her time a bit
 
-		it also wants to show beautiness of colors!
-	**/
-	private void greetUser(){
-		//	TUtil.clearConsule(cursor);
-		TUtil.print("initializing vim!", Color.YELLOW);
-		TUtil.print("input file is : " + ourFile, Color.BLUE);
-		TUtil.print("the app is starting ", Color.GREEN);
-		ETCUtil.delay(2);
-	}
+	//////////////////////////////////////////////////
 
-	/**
-		set the command to ""
-		because we aplied it earlier and we should look for new command,
-		do you agree?
-	**/
-	private void resetCommand(){
-		command = "";
-	}
-
-	/**
-		its obvious!
-		apply the command
-		and reset it
-		and go to one key mode!
-	**/
-	private void applyAndResetLongCommand(){
-		applyLongCommand();
+	private void goToStatisticsMode(){
+		Logger.log("enterring statistics mode");
 		resetCommand();
-		goToOneKeyCommandMode();
+		mode = EditorMode.STATISTICS;
+		//TODO : show statistics
 	}
+
 
 
 	private void applyLongCommand(){
@@ -155,57 +131,6 @@ public class Vim{
 		}
 	}
 
-	/**
-		handle the movement of cursor
-		with the given input character
-		note that up and down and right and left are not really up,down ,.. ensi chars
-		because ansi use 3 char to represent arow keys
-		so we used : sum of ord of 3 characters + 10
-		why? check out 193 - 196 on ascii extended table
-	**/
-	public void handleMove(char input){
-
-		switch (input) {
-			case up:
-				cursor.up();
-				break;
-			case down:
-				cursor.down();
-				break;
-			case right:
-			 	cursor.right();
-				break;
-			case left:
-			 	cursor.left();
-				break;
-		}
-	}
-
-
-	private void goToInsertMode(){
-		Logger.log("enterring insert mode");
-		resetCommand();
-		mode = EditorMode.INSERT;
-	}
-
-	private void goToOneKeyCommandMode(){
-		Logger.log("enterring insert one key mode");
-		resetCommand();
-		mode = EditorMode.ONE_KEY_COMMAND;
-	}
-
-	private void goToOneLongCommandMode(){
-		Logger.log("enterring long command mode");
-		resetCommand();
-		mode = EditorMode.LONG_COMMAND;
-	}
-
-	private void goToStatisticsMode(){
-		Logger.log("enterring statistics mode");
-		resetCommand();
-		mode = EditorMode.STATISTICS;
-		//TODO : show statistics
-	}
 
 
 	private void handleOneCharCommand(char inputC){
@@ -261,7 +186,7 @@ public class Vim{
 	}
 
 	/**
-	 main function and main loop
+	main function and main loop
 	**/
 	public void run(){
 
@@ -269,7 +194,7 @@ public class Vim{
 			char input = TUtil.getChar();
 			screen.printLine(cursor);
 
-			handleMove(input);
+			handleCursorMove(input);
 
 			switch(mode){
 
@@ -296,8 +221,102 @@ public class Vim{
 
 	} // end function run
 
+
+
+
+
+
+
 	/**
-	 cleanup and get ready to exit after a loop
+	this method do nothing except greeting user and
+	waste his/her time a bit
+
+	it also wants to show beautiness of colors!
+	**/
+	private void greetUser(){
+		//	TUtil.clearConsule(cursor);
+		TUtil.print("initializing vim!", Color.YELLOW);
+		TUtil.print("input file is : " + ourFile, Color.BLUE);
+		TUtil.print("the app is starting ", Color.GREEN);
+		ETCUtil.delay(2);
+	}
+
+	/**
+	set the command to ""
+	because we aplied it earlier and we should look for new command,
+	do you agree?
+	**/
+	private void resetCommand(){
+		command = "";
+	}
+
+	/**
+	its obvious!
+	apply the command
+	and reset it
+	and go to one key mode!
+	**/
+	private void applyAndResetLongCommand(){
+		applyLongCommand();
+		resetCommand();
+		goToOneKeyCommandMode();
+	}
+
+
+
+
+	/**
+	handle the movement of cursor
+	with the given input character
+	note that up and down and right and left are not really up,down ,.. ensi chars
+	because ansi use 3 char to represent arow keys
+	so we used : sum of ord of 3 characters + 10
+	why? check out 193 - 196 on ascii extended table
+	**/
+	public void handleCursorMove(char input){
+
+		switch (input) {
+			case up:
+			cursor.up();
+			break;
+			case down:
+			cursor.down();
+			break;
+			case right:
+			cursor.right();
+			break;
+			case left:
+			cursor.left();
+			break;
+		}
+	}
+
+
+
+
+	private void goToInsertMode(){
+		Logger.log("enterring insert mode");
+		resetCommand();
+		mode = EditorMode.INSERT;
+	}
+
+	private void goToOneKeyCommandMode(){
+		Logger.log("enterring insert one key mode");
+		resetCommand();
+		mode = EditorMode.ONE_KEY_COMMAND;
+	}
+
+	private void goToOneLongCommandMode(){
+		Logger.log("enterring long command mode");
+		resetCommand();
+		mode = EditorMode.LONG_COMMAND;
+	}
+
+
+
+
+	/**
+	cleanup and get ready to exit after a loop
 	**/
 	public void exit(){
 		cleanUpForExit();
@@ -313,31 +332,14 @@ public class Vim{
 	}
 
 	/**
-		our lovely main mathod!
-		it just create new instance of vim and run it
+	our lovely main mathod!
+	it just create new instance of vim and run it
 	**/
 	public static void main(String[] args){
 		Vim app = new Vim(args);
 		app.run();
 	}
 }
-
-
-/*
-
-command mode :
-:wq
-:w
-:q
-:q!
-h j k l
-
-editor mode :
-esacpe + [A  [B [C [D
-up,down, right, left
-
-
-*/
 
 
 
